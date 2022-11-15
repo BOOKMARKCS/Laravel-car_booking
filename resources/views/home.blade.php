88f447c0cbf03c6bdc2d6531a4647cdcaff984e9
markman <NaymarkCK.44@gmail.com>
2022-11-15 20:33:36 +0700

    install tailwindcss

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline bg-red-700">
+    Hello world!
+  </h1>
+  <p class="text-tahiti-400/100">The quick brown fox...</p>
+  <p class="text-sky-500">Text-sky</p>
+  <p class="bg-silver">eiffel;iug</p>
+
+
+  <button class="bg-indigo-500 ...">
+  Save changes
+</button>
+</body>
+</html>
